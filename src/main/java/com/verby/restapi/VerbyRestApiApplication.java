931a5c3ddba58e1a27adf198260b7eca560740fe
Jerry(이재헌) <ti6419@gmail.com>
2022-07-16 15:26:42 +0900

    Initial commit

---
+++ src/main/java/com/verby/restapi/VerbyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.verby.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verby/restapi/VerbyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verby.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
