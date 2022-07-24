cbf5297fbcacb9c3ccfe27b1a6fdb3881d72ecf3
Jerry(이재헌) <ti6419@gmail.com>
2022-07-24 18:33:23 +0900

    feat: Rest-docs 적용 (+ Customize snippet)

--- src/test/java/com/verby/restapi/account/presentation/AccountControllerTest.java
+++ src/test/java/com/verby/restapi/account/presentation/AccountControllerTest.java
@@ -8,10 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.verby.restapi.common.presentation.ApiDocumentUtils.getDocumentRequest;
+import static com.verby.restapi.common.presentation.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,21 +35,39 @@ class AccountControllerTest extends BasicControllerTest {
     @Test
     @DisplayName("유저를 생성할 수 있다.")
     void signup() throws Exception {
+        // given
         SignUpRequest signUpRequest = SignUpRequest.builder()
-                .loginId("TestId")
-                .password("TestPassword")
+                .loginId("test123")
+                .password("password12@")
                 .name("TestName")
                 .birthday(LocalDateTime.now())
                 .phone("01012345678")
                 .allowToMarketingNotification(true)
                 .build();
 
-        mockMvc.perform(post("/accounts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(status().isCreated())
+        // when
+        ResultActions result = mockMvc.perform(post("/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)));
+
+        // then
+        result.andExpect(status().isCreated())
                 .andExpect(jsonPath("login_id").value(signUpRequest.getLoginId()))
-               ;
+                .andDo(document("계정 등록",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("login_id").type(JsonFieldType.STRING).description("계정 로그인 ID"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("계정 비밀번호"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("phone").type(JsonFieldType.STRING).description("전화번호"),
+                                fieldWithPath("birthday").type(JsonFieldType.STRING).description("생년월일").optional(),
+                                fieldWithPath("allow_to_marketing_notification").type(JsonFieldType.BOOLEAN).description("마케팅 정보 수신 동의 여부")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("계정 일련번호"),
+                                fieldWithPath("login_id").type(JsonFieldType.STRING).description("계정 로그인 ID")
+                        )));
     }
 
 
--- src/test/java/com/verby/restapi/common/presentation/BasicControllerTest.java
+++ src/test/java/com/verby/restapi/common/presentation/BasicControllerTest.java
@@ -2,12 +2,14 @@ package com.verby.restapi.common.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class BasicControllerTest {
 
     @Autowired
