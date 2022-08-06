cc4a023c3d5abcb0543e026601368b5020b0a2dc
Jerry(이재헌) <ti6419@gmail.com>
2022-08-06 23:09:22 +0900

    style: 사용하지 않는 throws 문 제거

--- src/main/java/com/verby/restapi/config/security/api/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/verby/restapi/config/security/api/AuthenticationSuccessHandlerImpl.java
@@ -5,16 +5,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         response.setStatus(HttpStatus.NO_CONTENT.value());
     }
 
