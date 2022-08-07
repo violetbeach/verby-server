17550d87a835a9e5060acc8bd26bfd1677f8b640
Jerry(이재헌) <ti6419@gmail.com>
2022-08-07 13:35:57 +0900

    chore: Remove unused exception

--- src/main/java/com/verby/restapi/common/error/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.verby.restapi.common.error.exception;
-
-import com.verby.restapi.common.error.ErrorCode;
-
-public class UnAuthorizedException extends BusinessException {
-    public UnAuthorizedException(String message) {
-        super(ErrorCode.UNAUTHORIZED, message);
-    }
-}
