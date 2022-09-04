ece2382589ccf7fa23e3f3ab5e3d14f7ae234f3f
Jerry(이재헌) <ti6419@gmail.com>
2022-09-04 22:21:13 +0900

    chore: (Draft) Error Response Test 추가

---
+++ src/test/java/com/verby/restapi/common/error/presentation/ErrorResponseDocumentationTest.java
@@ -0,0 +1,9 @@
+package com.verby.restapi.common.error.presentation;
+
+import com.verby.restapi.common.presentation.BaseControllerTest;
+
+public class ErrorResponseDocumentationTest extends BaseControllerTest {
+
+    // TODO GET /covers/{id}로 추가 예정
+
+}
