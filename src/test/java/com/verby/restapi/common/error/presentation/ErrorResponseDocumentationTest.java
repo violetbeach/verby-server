18d8e7915e7d4c32e9b571329b90cc00dc4c8992
Jerry(이재헌) <ti6419@gmail.com>
2022-09-21 22:40:36 +0900

    docs: ErrorResponseDocumentation 추가

--- src/test/java/com/verby/restapi/common/error/presentation/ErrorResponseDocumentationTest.java
+++ src/test/java/com/verby/restapi/common/error/presentation/ErrorResponseDocumentationTest.java
@@ -1,9 +1,39 @@
 package com.verby.restapi.common.error.presentation;
 
 import com.verby.restapi.common.presentation.BaseControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static com.verby.restapi.common.presentation.ApiDocumentUtils.getDocumentRequest;
+import static com.verby.restapi.common.presentation.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ErrorResponseDocumentationTest extends BaseControllerTest {
 
-    // TODO GET /covers/{id}로 추가 예정
+    @Test
+    @DisplayName("에러 응답 Documentation")
+    void errorResponseDocumentation() throws Exception {
+        // when
+        ResultActions result = mockMvc.perform(get("/users/me"));
+
+        // then
+        result.andExpect(status().isUnauthorized());
+
+        // docs
+        result.andDo(document("에러 응답",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                responseFields(
+                        fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지")
+                ))
+        );
+    }
 
 }
