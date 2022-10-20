6424d983ac94f233152b572d9075c03399eec64e
Jerry(이재헌) <ti6419@gmail.com>
2022-10-20 21:53:09 +0900

    feat: HEAD /users (param 추가 -> phone)

--- src/main/java/com/verby/restapi/user/presentation/UserController.java
+++ src/main/java/com/verby/restapi/user/presentation/UserController.java
@@ -4,7 +4,9 @@ import com.verby.restapi.common.error.ErrorCode;
 import com.verby.restapi.common.error.exception.EntityNotFoundException;
 import com.verby.restapi.config.security.SecurityUser;
 import com.verby.restapi.user.query.dao.UserDataDao;
+import com.verby.restapi.user.query.dao.UserDataQueryDao;
 import com.verby.restapi.user.query.dto.UserData;
+import com.verby.restapi.user.query.dto.UserSearchParam;
 import com.verby.restapi.user.query.dto.UserSummary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,10 +19,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserDataDao userDataDao;
+    private final UserDataQueryDao userDataQueryDao;
 
     @RequestMapping(method = RequestMethod.HEAD)
-    private ResponseEntity<UserData> existsLoginId(@RequestParam(value = "login-id") String loginId) {
-        UserData userData = userDataDao.findByLoginId(loginId)
+    private ResponseEntity<UserData> existsUser(UserSearchParam searchParam) {
+        UserData userData = userDataQueryDao.searchOne(searchParam)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND, "Not found."));
         return new ResponseEntity<>(userData, HttpStatus.OK);
     }
