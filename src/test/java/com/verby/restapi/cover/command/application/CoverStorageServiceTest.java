5d4522c2795e08ef8d84e0d95e9dbb315b5b7f78
Jerry(이재헌) <ti6419@gmail.com>
2022-12-09 14:25:37 +0900

    test: CoverStorageService 테스트 추가

--- src/test/java/com/verby/restapi/cover/command/application/CoverStorageServiceTest.java
+++ src/test/java/com/verby/restapi/cover/command/application/CoverStorageServiceTest.java
@@ -1,7 +1,50 @@
 package com.verby.restapi.cover.command.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.verby.restapi.common.storage.dto.Resource;
+import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class CoverStorageServiceTest {
+
+    CoverStoragePathProperties coverStoragePathProperties = new CoverStoragePathProperties(
+            "/video", "/highlight", "/image"
+    );
+
+    CoverStorageService coverStorageService = new MockCoverStorageService(coverStoragePathProperties);
+
+    @ParameterizedTest
+    @MethodSource("paramsForGetPreSignUrl")
+    void getPreSignUrl(Resource resource) {
+        // when
+        coverStorageService.getResourcePath(resource);
+
+        // then
+        // - Not Throw Exception
+    }
+
+
+    static class MockCoverStorageService extends CoverStorageService {
+        public MockCoverStorageService(CoverStoragePathProperties storagePathProperties) {
+            super(storagePathProperties);
+        }
+
+        @Override
+        public String getPreSignedUrl(Resource resource) {
+            getResourcePath(resource);
+            return Strings.EMPTY;
+        }
+    }
+
+    static Stream<Arguments> paramsForGetPreSignUrl() {
+        return Stream.of(
+                Arguments.of(Resource.FULL_VIDEO),
+                Arguments.of(Resource.HIGHLIGHT),
+                Arguments.of(Resource.IMAGE)
+        );
+    }
 
 }
