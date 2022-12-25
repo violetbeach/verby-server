566e41bbc82c4f0ae791d48b2d52c3d02f70bd66
Jerry(이재헌) <ti6419@gmail.com>
2022-12-25 20:53:35 +0900

    chore: core 모듈 RunApplication 제거

--- core/src/test/java/verby/core/CoreApplicationTests.java
+++ core/src/test/java/verby/core/CoreApplicationTests.java
@@ -1,9 +1,9 @@
 package verby.core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class CoreApplicationTests {
 
     @Test
