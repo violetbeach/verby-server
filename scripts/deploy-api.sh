62d72be639f03a81e6aa6704fb40eb2b125e530b
Jerry(이재헌) <ti6419@gmail.com>
2022-12-25 15:58:15 +0900

    chore: AWS code deploy 스크립트 수정 (멀티 모듈 반영)

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/deploy/api-server
+REPOSITORY=/home/ec2-user/app/deploy/build/api-server
 
 CURRENT_PID=$(pgrep -f api-server)
 
@@ -17,10 +17,10 @@ fi
 echo "> 새 어플리케이션 배포"
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar/
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/jar/
 
 
-JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'restapi' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'api-server' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
