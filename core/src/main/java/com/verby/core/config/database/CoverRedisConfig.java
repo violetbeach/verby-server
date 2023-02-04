4c35f6c770a46f222164a1086862f4a25eaa7862
Jerry(이재헌) <ti6419@gmail.com>
2023-02-05 00:47:46 +0900

    fix: CoverQuery 레디스 Repository 사용 제거 (Mongo로 변경)

--- core/src/main/java/com/verby/core/config/database/CoverRedisConfig.java
+++ core/src/main/java/com/verby/core/config/database/CoverRedisConfig.java
@@ -1,7 +1,6 @@
 package com.verby.core.config.database;
 
 import com.verby.core.cover.command.domain.CoverHitRepository;
-import com.verby.core.cover.query.dao.CoverQueryDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
@@ -20,7 +19,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
-@EnableRedisRepositories(basePackageClasses = {CoverQueryDao.class, CoverHitRepository.class})
+@EnableRedisRepositories(basePackageClasses = {CoverHitRepository.class})
 @RequiredArgsConstructor
 public class CoverRedisConfig {
 
