673c996e73d9f5d0bae6bcdc9a8c7dd1eae1b15d
Jerry(이재헌) <ti6419@gmail.com>
2023-02-05 01:11:56 +0900

    fix typo (오타 수정 collation -> collection)

--- core/src/main/java/com/verby/core/cover/query/dto/CoverQueryModel.java
+++ core/src/main/java/com/verby/core/cover/query/dto/CoverQueryModel.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
-@Document(collation = "cover")
+@Document(collection = "cover")
 @AllArgsConstructor
 public class CoverQueryModel {
     @Id
