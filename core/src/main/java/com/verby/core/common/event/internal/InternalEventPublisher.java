93e04cc800248abfc9530f5fec1644b20d325df6
Jerry(이재헌) <ti6419@gmail.com>
2023-02-18 22:10:34 +0900

    refactor: InternalEventPublisher에 DIP 적용 (EventHandler의 외부 의존성 제거)

--- core/src/main/java/com/verby/core/common/event/internal/InternalEventPublisher.java
+++ core/src/main/java/com/verby/core/common/event/internal/InternalEventPublisher.java
@@ -1,21 +1,9 @@
 package com.verby.core.common.event.internal;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.stereotype.Service;
+import com.verby.core.common.event.internal.infra.InternalEventTopic;
 
-@Service
-public class InternalEventPublisher {
+public interface InternalEventPublisher {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public InternalEventPublisher(@Qualifier("messageRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
-    public void publish(ChannelTopic topic, Long message) {
-        redisTemplate.convertAndSend(topic.getTopic(), message);
-    }
+    public void publish(InternalEventTopic topic, Object message);
 
 }
