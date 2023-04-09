79e37c79ac1b3c1f20298b715af47fc6fcb94401
Jerry(이재헌) <ti6419@gmail.com>
2023-04-09 19:20:21 +0900

    chore: CodeDeploy 미동작 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# $MODULES 환경 변수를 읽어와서 $MODULES_LIST 배열로 변환
-IFS=',' read -r -a MODULES_LIST <<< "$MODULES"
-
-# $MODULES_LIST 배열을 순회하며 각 모듈을 처리
-for MODULE in "${MODULES_LIST[@]}"
-do
-  # 변경된 모듈에 대한 작업 수행
-  deploy_module($MODULE)
-done
+# 변경된 모듈에 대한 작업 수행
+deploy_module($MODULE)
 
 function deploy_module {
-  local repository_path="/home/ec2-user/app/deploy/build/${MODULE}"
+  local repository_path="/home/ec2-user/deploy"
   local current_pid=$(pgrep -f "${MODULE}")
 
   echo "$current_pid"
@@ -28,14 +21,11 @@ function deploy_module {
 
   echo "> Build 파일 복사"
 
-  mkdir "$repository_path/jar"
-  cp "$repository_path/build/libs/"*.jar "$repository_path/jar/"
-
-  jar_name=$(ls "$repository_path/jar/" | grep "${MODULE}" | head -n 1)
+  jar_name=$(ls "$repository_path/" | grep "${MODULE}" | head -n 1)
 
   echo "> JAR Name: $jar_name"
 
   source /home/ec2-user/.bash_profile
 
-  nohup /opt/jdk-17/bin/java -jar "$repository_path/jar/$jar_name" --spring.profiles.active=prod > "$repository_path/nohup.out" 2>&1 &
+  nohup /opt/jdk-17/bin/java -jar "$repository_path/$jar_name" --spring.profiles.active=prod > "$repository_path/nohup.out" 2>&1 &
 }
