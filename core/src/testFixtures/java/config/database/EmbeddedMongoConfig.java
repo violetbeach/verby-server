28f708c043c5c20ea0c8fb27765f7b9bf1aaac40
Jerry(이재헌) <ti6419@gmail.com>
2023-04-17 22:30:14 +0900

    refactor: Disabled 테스트 사용하도록 변경

--- core/src/testFixtures/java/config/database/EmbeddedMongoConfig.java
+++ core/src/testFixtures/java/config/database/EmbeddedMongoConfig.java
@@ -1,10 +1,11 @@
 package config.database;
 
+import com.verby.core.cover.query.dao.CoverQueryDao;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import support.repository.TestCoverQueryModelRepository;
 
 @Configuration
-@EnableMongoRepositories(basePackageClasses = TestCoverQueryModelRepository.class)
+@EnableMongoRepositories(basePackageClasses = {TestCoverQueryModelRepository.class, CoverQueryDao.class})
 public class EmbeddedMongoConfig {
 }
